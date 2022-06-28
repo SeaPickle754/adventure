405c4268b489d6fda7d94aecc394c046397c81f4
SeaPickle <uraniumpjs@gmail.com>
2022-06-28 11:53:31 -0600

    fixed player movement glitch

--- Desktop/adventure/main.py
+++ Desktop/adventure/main.py
@@ -1,15 +1,16 @@
 import curses
 from miscclasses import *
-import logging
 
 screen = curses.initscr()
 screen.clear()
-curses.noecho()
+#curses.noecho()
 screen.keypad(True)
 curses.cbreak()
+
 curses.start_color()
 curses.curs_set(0)
 player = Player()
+
 curses.init_pair(1, curses.COLOR_GREEN, curses.COLOR_BLACK)
 curses.init_pair(2, curses.COLOR_RED, curses.COLOR_BLACK)
 
@@ -29,28 +30,31 @@ def draw():
 				screen.addch(y, x, "#", curses.color_pair(1))
 def doEvents():
 	key=screen.getch()
+
 	#TODO: fix player movement glitch
 	if key == ord('q'):
 		endApp()
 
-	if key == curses.KEY_UP:
+	elif key == curses.KEY_UP:
 		player.y -= 1
+		return
 
-	if key == curses.KEY_DOWN:
+	elif key == curses.KEY_DOWN:
 		player.y += 1
+		return
 	
-	if key == curses.KEY_LEFT:
+	elif key == curses.KEY_LEFT:
 		player.x -= 1
-	
-	if key == curses.KEY_RIGHT:
+		return
+	elif key == curses.KEY_RIGHT:
 		player.x += 1
-
+		return
 while 1:
+	doEvents()
 	screen.clear()
 	try:
 		draw()
 	except:
-		doEvents()
-	doEvents()
+		continue
 	screen.refresh()
 endApp()
